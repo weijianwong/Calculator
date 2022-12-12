382afd1dba951d68911859f70b96ed2eb53866f8
Wong Wei Jian <weijian9091@gmail.com>
2022-12-12 11:42:16 +0800

    Add the UI for the calculator and the basic javaScript functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Calculator
+
+Project for The Odin Project. This calculator includes basic math functions and it is coded using HTML, CSS and JavaScript.
---
+++ myScript.js
@@ -0,0 +1,20 @@
+function add(x,y){
+    return x + y;
+}
+
+function subtract(x,y){
+    return x - y;
+}
+
+function multiply(x,y){
+    return x * y;
+}
+
+function divide(x,y){
+    return x / y;
+}
+
+function operate(operator, x, y){
+    return operator(x,y);
+}
+
